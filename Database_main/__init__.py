2d5b882f8a83db1efbd84996df83342c790222bf
Ubuntu <azureuser@lab1.ereijag5ygaexd0hsbqan5qegc.fx.internal.cloudapp.net>
2025-10-09 17:57:33 +0000

    Commit changes

--- Database_main/__init__.py
+++ Database_main/__init__.py
@@ -4,13 +4,18 @@ from Database_main.database import db
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:pasle2006@localhost/moviedb"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc://CloudSA5110a274:12345iryna_@database-main.database.windows.net:1433/moviedb?driver=ODBC+Driver+17+for+SQL+Server"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
     with app.app_context():
         from Database_main.models.movie import Movie
+        from Database_main.models.actor import Actor
+        from Database_main.models.rating import Rating
+        from Database_main.models.user import User
+        from Database_main.models.genre import Genre
+        from Database_main.models.review import Review
         db.create_all()
 
     return app
